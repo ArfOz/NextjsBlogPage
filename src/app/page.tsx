1e53591116544ffab21dd98050931beec2ae79b8
Arif Trends <arif.ozturk@trendsbooking.com>
2023-06-20 23:55:30 +0200

    update link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+'use client'
+import { Metadata } from "next"
+import Link from "next/link"
+import { useRouter } from 'next/navigation'
+
+export const metadata: Metadata = {
+  title: 'Arif Özkan NextJS',
+}
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  const router = useRouter()
+  return (
+  <>
+  <h1>Hello, Next.js!</h1>
+  <ul>
+      
+      {/* <button type="button" onClick={() => router.push('/about')}>
+      About Me
+    </button> */}
+        <li>
+          <Link href={`/about`}>About Me</Link>
+        </li>
+    </ul>
+    </>
+    )
 }
