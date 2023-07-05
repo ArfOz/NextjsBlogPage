8884e15403d2ec06500485b1af4eb398b326530e
Arif Trends <arif.ozturk@trendsbooking.com>
2023-07-06 00:00:46 +0200

    fix response

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -1,16 +1,17 @@
 // import React from 'react';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { SendMail } from './nodemailer';
+import { NextResponse } from 'next/server';
 
 // import { NextResponse } from 'next/server';
-// export async function POST(req: NextApiRequest, res: NextApiResponse) {
-//     console.log('bodyyyyyyyyyy', req.body);
-//     // const res = await SendMail();
-//     console.log('ressssssss', res);
-//     return res.status(200).json({ message: 'Hello from Next.js!' });
-// }
-
-export async function POST(req, res) {
-    console.log('selammmm');
-    return res.json({ success: 'ok' });
+export async function POST(req: Request) {
+    console.log('bodyyyyyyyyyy', req.body);
+    // const res = await SendMail();
+    console.log('ressssssss', await req.json());
+    return NextResponse.json({ message: 'Hello from Next.js!' });
 }
+
+// export async function POST(request: Request) {
+//     console.log('selammmm', await request.json());
+//     return NextResponse.json({ hello: request.body });
+// }
