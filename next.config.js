03314da89d5dde08a62185074736eb5497301e69
Arif Trends <arif.ozturk@trendsbooking.com>
2023-07-16 22:50:33 +0200

    fix 1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const { i18n } = require('./next-i18next.config.js');
 const path = require('path');
+const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
