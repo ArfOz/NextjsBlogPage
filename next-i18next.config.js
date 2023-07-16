2c13ccab63a54b2b374fcd0c1a42deb8bdd4d728
Arif Trends <arif.ozturk@trendsbooking.com>
2023-07-16 15:16:13 +0200

    fix i18nxt and add react flag select

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,16 +1,6 @@
-const path = require("path");
-
 module.exports = {
     i18n: {
-        defaultLocale: "en",
-        locales: ["en", "tr"],
-        localePath: "./public/locales",
-        localeExtension: "json",
-        localeStructure: "{{lng}}/{{ns}}",
-        /** To avoid issues when deploying to some paas (vercel...) */
-        localePath:
-            typeof window === "undefined"
-                ? require("path").resolve("./public/locales")
-                : "/locales",
+        defaultLocale: 'en',
+        locales: ['en', 'tr'],
     },
 };
--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
+const { i18n } = require('./next-i18next.config.js');
 /** @type {import('next').NextConfig} */
-const { i18n } = require("./next-i18next.config.js");
 
 const nextConfig = {
-    pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+    pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
     reactStrictMode: true,
+    i18n,
     experimental: {
         appDir: true,
     },
-    i18n,
 };
 
 module.exports = nextConfig;
