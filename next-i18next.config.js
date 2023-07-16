adcd6ed88e8638328f46d910698f233f8bed9498
Arif Trends <arif.ozturk@trendsbooking.com>
2023-07-17 00:24:55 +0200

    add i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,8 @@
+const path = require('path');
 module.exports = {
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'tr'],
+        localePath: path.resolve('./public/static/locales'),
     },
 };
