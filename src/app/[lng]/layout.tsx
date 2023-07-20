e143eb07109dc823c82bc3642c80c45225cfa9a4
Arif Trends <arif.ozturk@trendsbooking.com>
2023-07-20 23:20:29 +0200

    update todos

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -5,9 +5,17 @@ import Navbar from './components/navbar';
 import Footer from './components/footer';
 import { dir } from 'i18next';
 import { languages } from '@/i18n/settings';
+import { Head } from 'next/document';
+import { useTranslation } from '@/i18n';
+import { use } from 'react';
 
 export async function generateStaticParams() {
     return languages.map((lng) => ({ lng }));
+}
+
+async function getTrans(lng: string) {
+    const { t } = await useTranslation(lng, 'common');
+    return t;
 }
 export default function RootLayout({
     children,
@@ -16,8 +24,12 @@ export default function RootLayout({
     children: React.ReactNode;
     params: { lng: string };
 }) {
+    const t = use(getTrans(lng));
     return (
         <html lang={lng} dir={dir(lng)}>
+            <head>
+                <title>{t('title')}</title>
+            </head>
             <body>
                 <ThemeProvider enableSystem={true} attribute="class">
                     <Navbar lng={lng} />
--- src/app/[lng]/todos/page.tsx
+++ src/app/[lng]/todos/page.tsx
@@ -19,15 +19,12 @@ export default function Todos({
     return (
         <div className="p-4 mb-16">
             <ul className="list-disc mx-4">
-                <li>{t('title')}</li>
+                -------------------English-------------------
                 <li>Photos</li>
                 <li>Social Login</li>
-                <li>Email to my email adress</li>
-                <li>Contact(Footer)</li>
                 <li>
                     Update with nextjs features(Loading page, error page ....)
                 </li>
-                <li>Multi language support</li>
             </ul>
         </div>
     );
