8ff66294e872c8890cd501e0e299d3fbc9e1807a
Arif Trends <arif.ozturk@trendsbooking.com>
2023-08-01 23:44:53 +0200

    update image src

--- src/app/[lang]/components/profilePic.tsx
+++ src/app/[lang]/components/profilePic.tsx
@@ -4,7 +4,7 @@ export default function MyProfilePic() {
         <section className="w-full mx-auto">
             <Image
                 className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-                src="/images/profileLinkedin.jpg"
+                src={'/images/profileLinkedin.jpg'}
                 width={200}
                 height={200}
                 alt="Arif Özkan"
