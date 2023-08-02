4fc49018efb70fc3db003f345045e63e4177ff69
Arif Trends <arif.ozturk@trendsbooking.com>
2023-08-02 22:24:16 +0200

    add cors headers

--- next.config.js
+++ next.config.js
@@ -7,36 +7,6 @@ const nextConfig = {
         appDir: true,
         serverActions: true,
     },
-
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: '/api/:path*',
-                headers: [
-                    // omitted for brevity...
-                ],
-            },
-            {
-                source: '/api/special-data',
-                headers: [
-                    { key: 'Access-Control-Allow-Credentials', value: 'false' },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: 'https://example.com',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,DELETE,PATCH,POST,PUT',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -3,13 +3,14 @@ import { MailData } from './types';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { SendMail } from './nodemailer';
 import { NextResponse } from 'next/server';
+import { corsHeaders } from '@/cors';
 
 // import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
     const data: MailData = await req.json();
     const res = await SendMail(data);
 
-    return NextResponse.json({ Data: res });
+    return NextResponse.json({ Data: res }, { headers: corsHeaders });
 }
 
 // export async function POST(request: Request) {
