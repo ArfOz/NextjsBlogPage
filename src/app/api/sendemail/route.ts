8dc35bdbc7e49517f51bc1854cdffe8412877fd6
Arif Trends <arif.ozturk@trendsbooking.com>
2023-08-02 22:30:14 +0200

    update cors

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -10,7 +10,18 @@ export async function POST(req: Request) {
     const data: MailData = await req.json();
     const res = await SendMail(data);
 
-    return NextResponse.json({ Data: res }, { headers: corsHeaders });
+    return NextResponse.json(
+        { Data: res },
+        {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods':
+                    'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials': 'true',
+            },
+        }
+    );
 }
 
 // export async function POST(request: Request) {
