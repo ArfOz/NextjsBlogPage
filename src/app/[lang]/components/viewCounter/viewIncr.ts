6f3bf65561650b7ed6a61e085a102b910046d48f
Arif Trends <arfoz1245@gmail.com>
2023-08-13 15:24:54 +0200

    add config

--- src/app/[lang]/components/viewCounter/viewIncr.ts
+++ src/app/[lang]/components/viewCounter/viewIncr.ts
@@ -2,17 +2,6 @@
 import React, { use, useEffect } from 'react';
 import { Locale } from 'i18n-config';
 
-// const view = async (lang: any) => {
-//     await fetch('/api/incr', {
-//         body: JSON.stringify(lang),
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         method: 'POST',
-//     });
-//     return null;
-// };
-
 export const ReportIncr = ({ lang }: { lang: Locale }) => {
     useEffect(() => {
         fetch('/api/incr', {
