74affa698a1efe19fe3cd24c2f3c3ed1ed67a7eb
Arif Trends <arfoz1245@gmail.com>
2023-12-28 22:42:25 +0100

    change experience component

--- src/app/[lang]/components/Experience/experience.tsx
+++ src/app/[lang]/components/Experience/experience.tsx
@@ -12,80 +12,46 @@ import { MdWorkOutline } from 'react-icons/md'
 import { FaSchool } from 'react-icons/fa'
 import { TbTank } from 'react-icons/tb'
 
-const workIcon = {
-    icon: <MdWorkOutline />,
-    iconStyle: { background: 'rgb(33, 150, 243)', color: '#fff' },
-}
-
-const schoolIcon = {
-    icon: <FaSchool />,
-    iconStyle: { background: 'rgb(233, 30, 99)', color: '#fff' },
-}
-const armyIcon = {
-    icon: <TbTank />,
-    iconStyle: { background: 'rgb(16, 204, 82)', color: '#fff' },
+function iconSelector(icon: string) {
+    switch (icon) {
+        case 'workIcon':
+            return <MdWorkOutline />
+        case 'schoollIcon':
+            return <FaSchool />
+        case 'armyIcon':
+            return <FaSchool />
+        default:
+            return <FaSchool />
+    }
 }
 
 export function Experience({ dictionary }: { dictionary: any }) {
     return (
-        <section id="experience" className="full">
-            <h3>
-                My Experience{' '}
-                {/* <a
-                    href="https://www.cluemediator.com/"
-                    target="_blank"
-                    rel="noopener"
-                >
-                    My Experience
-                </a> */}
-            </h3>
-
-            <VerticalTimeline>
-                {dictionary['experience'].map((t: any, i: any) => {
-                    const contentStyle =
-                        i === 0
-                            ? { background: 'rgb(33, 150, 243)', color: '#fff' }
-                            : undefined
-                    const arrowStyle =
-                        i === 0
-                            ? { borderRight: '7px solid  rgb(33, 150, 243)' }
-                            : undefined
-
-                    console.log('t', t)
-                    console.log('i', i)
-                    return (
-                        <VerticalTimelineElement
-                            visible={true}
-                            key={i}
-                            className="vertical-timeline-element--work"
-                            contentStyle={contentStyle}
-                            contentArrowStyle={arrowStyle}
-                            date={t.date}
-                            icon={<MdWorkOutline />}
-                        >
-                            {t.title ? (
-                                <React.Fragment>
-                                    <h3 className="vertical-timeline-element-title">
-                                        {t.title}
-                                        {' - '}
-                                        <span className="vertical-timeline-element-title font-bold">
-                                            {t.company}
-                                        </span>
-                                    </h3>
-
-                                    <h4 className="vertical-timeline-element-subtitle text-sm">
-                                        {t.years}
-                                    </h4>
-
-                                    <p className="vertical-timeline-element-subtitle font-thin ">
-                                        {t.technologies.join(', ')}
-                                    </p>
-                                </React.Fragment>
-                            ) : undefined}
-                        </VerticalTimelineElement>
-                    )
-                })}
-            </VerticalTimeline>
-        </section>
+        <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+            {dictionary['experience'].map((t: any, i: any) => {
+                console.log(`${t.icon}`)
+                return (
+                    <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+                        <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-slate-400 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+                            {iconSelector(t.icon)}
+                            {/* {workIcon} */}
+                        </div>
+                        <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+                            <div className="flex items-center justify-between space-x-2 mb-1">
+                                <div className="font-bold text-slate-900">
+                                    {t.title} - {t.company}
+                                </div>
+                                <time className="font-caveat font-medium text-indigo-500">
+                                    {t.years}
+                                </time>
+                            </div>
+                            <div className="text-slate-500">
+                                {t.technologies.join(', ')}
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
