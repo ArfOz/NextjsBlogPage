b753271ced00beb537d1237d2b979e6d3fbe536c
Arif Trends <arfoz1245@gmail.com>
2024-08-06 23:36:16 +0200

    start weather footer

--- src/libs/openweather.ts
+++ src/libs/openweather.ts
@@ -1,21 +1,21 @@
-const api_key = process.env.OPENWEATHER_API!;
+const api_key = process.env.OPENWEATHER_API!
 var requestOptions: RequestInit = {
     method: 'GET',
     redirect: 'follow',
     cache: 'no-store',
-};
+}
 
 export async function OpenWeather(id: number, lang: string) {
     try {
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${api_key}&units=metric&lang=${lang}`,
             requestOptions
-        );
-        const data = response.json();
+        )
+        const data = response.json()
 
-        return data;
+        return data
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
 
@@ -24,11 +24,24 @@ export async function OpenWeatherIcon(id: string) {
         const response = await fetch(
             `https://openweathermap.org/img/wn/${id}@2x.png`,
             requestOptions
-        );
-        const data = response.json();
+        )
+        const data = response.json()
 
-        return data;
+        return data
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
+}
+
+export async function WeatherData(Cities: any, lang: any) {
+    let promises = []
+    for (let index = 0; index < Cities.length; index++) {
+        let city = Cities[index]
+        let tmpPromise = OpenWeather(city.Id, lang)
+        promises.push(tmpPromise)
+    }
+
+    let data = await Promise.all(promises)
+
+    return data
 }
