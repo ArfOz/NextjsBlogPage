54d8a0ba2cfa2727c8ad2aba898282c5fe48a430
Arif Trends <arfoz1245@gmail.com>
2024-08-09 00:01:24 +0200

    add usestate openweather interface

--- src/app/[lang]/components/weatherCard/weathernavbar.tsx
+++ src/app/[lang]/components/weatherCard/weathernavbar.tsx
@@ -3,16 +3,17 @@ import React, { useEffect, useState } from 'react'
 import Cities from '@/libs/cities.json'
 import { OpenWeather } from '@/libs/openweather'
 import { Locale } from 'i18n-config'
+import { OpenWeatherResponse } from '../types'
 
 export function Weathernavbar({ lang }: { lang: Locale }) {
     // To reduce take instant weather forecast multiple cities after for promise all
-    const [weather, setWeather] = useState('...')
+    const [weather, setWeather] = useState<OpenWeatherResponse>()
 
     useEffect(() => {
         async function getWeather(lang: Locale) {
             let city = Cities[0]
             console.log('useeffect', city.Id, lang)
-            const data = (await OpenWeather(city.Id, lang)).toString()
+            const data = await OpenWeather(city.Id, lang)
 
             console.log('ic', data)
 
@@ -21,6 +22,6 @@ export function Weathernavbar({ lang }: { lang: Locale }) {
         getWeather(lang)
     }, [lang])
 
-    console.log('ariff', weather)
-    return <div>{weather}</div>
+    console.log('ariff', weather?.main?.temp)
+    return <div>{weather?.main?.temp}</div>
 }
