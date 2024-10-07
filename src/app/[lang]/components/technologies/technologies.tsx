9e444829972d4f92e277a3cae3d6381b78ee1a86
Arif Trends <arfoz1245@gmail.com>
2024-10-07 12:16:08 +0200

    center hover element

--- src/app/[lang]/components/technologies/technologies.tsx
+++ src/app/[lang]/components/technologies/technologies.tsx
@@ -4,9 +4,18 @@ import { SvgIcon } from '../icons'
 
 const Technologies = ({ techs }: { techs: DictionaryType['technologies'] }) => {
     return (
-        <div className="container mx-auto p-4 ">
-            <div className="text-center font-bold">Technologies</div>
-            <div className="flex flex-row space-x-8 flex-wrap justify-center items-center space-y-8">
+        <div className="container mx-auto p-4 flex flex-col md:flex-row">
+            <div className="text-center font-bold flex-1">
+                Technologies Lorem ipsum, dolor sit amet consectetur adipisicing
+                elit. Sequi illum excepturi ratione aspernatur, eligendi,
+                tenetur magni dolore quod nobis reprehenderit ad perferendis
+                voluptatem aperiam quam blanditiis. Neque eveniet aperiam a!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Aliquam praesentium, fugit quam laborum sapiente magni accusamus
+                delectus? Nobis officia dolorem nulla vero doloribus. Sed
+                necessitatibus quibusdam aspernatur nostrum incidunt eveniet.
+            </div>
+            <div className="flex flex-row flex-wrap place-content-center place-items-center flex-1 justify-evenly gap-y-6">
                 {techs.map((tech: string) => {
                     const IconComponent = SvgIcon(tech, true)
                     return IconComponent
